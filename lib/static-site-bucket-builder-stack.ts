b15dc78a389effc0dccf027e7e314e8c1994b3a7
Jordan Paris <hamologist@gmail.com>
2023-03-10 23:08:20 -0500

    Initialization and basic implementation
    Current iteration will simply take everything in the "deploy" directory,
    and upload it to a S3 bucket with publicReadAccess. User has to provide
    a CFN parameter that determines the name of the bucket to create and
    upload to.

---
+++ lib/static-site-bucket-builder-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+
+export class StaticSiteBucketBuilderStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const staticSiteBucketParameter = new cdk.CfnParameter(this, "staticSiteBucketName", {
+      type: "String",
+      description: "The name of the Amazon S3 bucket where the static site will be deployed.",
+    });
+
+    const staticSiteBucket = new s3.Bucket(this, 'StaticSiteBucket', {
+      bucketName: staticSiteBucketParameter.valueAsString,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+    });
+
+    new s3Deployment.BucketDeployment(this, 'StaticSiteBucketDeployment', {
+      sources: [s3Deployment.Source.asset("./deploy")],
+      destinationBucket: staticSiteBucket,
+    });
+  }
+}
---
+++ test/static-site-bucket-builder.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StaticSiteBucketBuilder from '../lib/static-site-bucket-builder-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/static-site-bucket-builder-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StaticSiteBucketBuilder.StaticSiteBucketBuilderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
